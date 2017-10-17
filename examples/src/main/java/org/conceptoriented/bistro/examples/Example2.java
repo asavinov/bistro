5abc0c3a21d4a5d27cd631e9f9544fd783b83653
Alexandr Savinov <savinov@conceptoriented.org>
2017-10-17 20:39:53 +0200

    example 2

--- examples/src/main/java/org/conceptoriented/bistro/examples/Example2.java
+++ examples/src/main/java/org/conceptoriented/bistro/examples/Example2.java
@@ -25,13 +25,69 @@ public class Example2 {
         // Create tables and columns by loading data from CSV files
         //
 
-        //Double doubleObject = new Double("aaa");
-        //double number = Double.valueOf("nnn");
-        double number2 = Double.parseDouble("ccc");
+        Table columnType = schema.getTable("Object");
+
+        Table items = ExUtils.readFromCsv(schema, location, "OrderItems.csv");
 
         Table products = ExUtils.readFromCsv(schema, location, "Products.csv");
-        Table orderItems = ExUtils.readFromCsv(schema, location, "OrderItems.csv");
 
+        Table orders = schema.createTable("Orders");
+        schema.createColumn("ID", orders, columnType);
+
+        //
+        // Calculate amount
+        //
+
+        // [OrderItems].[Amount] = [Quantity] * [Unit Price]
+        Column itemsAmount = schema.createColumn("Amount", items, columnType);
+        itemsAmount.calc(
+                (p,o) -> Double.valueOf((String)p[0]) * Double.valueOf((String)p[1]),
+                items.getColumn("Quantity"), items.getColumn("Unit Price")
+        );
+
+        //
+        // Links from OrderItems to Products and Orders
+        //
+
+        // [OrderItems].[Product]: OrderItems -> Products
+        Column itemsProduct = schema.createColumn("Product", items, products);
+        itemsProduct.link(
+                new Column[] { products.getColumn("ID") },
+                items.getColumn("Product ID")
+        );
+
+        // [OrderItems].[Order]: OrderItems -> Orders
+        Column itemsOrder = schema.createColumn("Order", items, orders);
+        itemsProduct.link(
+                new Column[] { products.getColumn("ID") },
+                items.getColumn("Order ID")
+        );
+
+        //
+        // Accumulate item characteristics
+        //
+
+        // [Products].[Total Amount] = SUM [OrderItems].[Amount]
+        Column productsAmount = schema.createColumn("Total Amount", products, columnType);
+        productsAmount.setDefaultValue(0.0); // It will be used as an initial value
+        productsAmount.accu(
+                itemsProduct,
+                (p,o) -> Double.valueOf((String)p[0]) + Double.valueOf((String)o),
+                items.getColumn("Amount")
+        );
+
+        // [Order].[Total Amount] = SUM [OrderItems].[Amount]
+        Column ordersAmount = schema.createColumn("Total Amount", orders, columnType);
+        ordersAmount.setDefaultValue(0.0); // It will be used as an initial value
+        ordersAmount.accu(
+                itemsOrder, // [out] + [Quantity] * [Unit Price]
+                (p,o) -> Double.valueOf((String)p[0]) + Double.valueOf((String)o),
+                items.getColumn("Amount")
+        );
+
+        schema.eval();
+
+        int a = 0;
     }
 
 }
